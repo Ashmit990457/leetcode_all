746d7834311b3d6d9602ea88124a781cd0206805
Ashmit990457 <ashmitsingh061@gmail.com>
2026-08-16 21:08:13 +0530

    Time: 2 ms (65.61%) | Memory: 46.8 MB (28.37%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+       HashSet<Character>rows[]=new HashSet [9];
+        HashSet<Character>cols[]=new HashSet [9];
+        HashSet<Character>boxs[]=new HashSet [9];
+for(int i=0;i<9;i++){
+    rows[i]=new HashSet<>();
+    cols[i]=new HashSet<>();
+    boxs[i]=new HashSet<>();
+}
+for(int i=0;i<9;i++){
+    for(int j=0;j<9;j++){
+        char c=board[i][j];
+        if(c=='.'){
+            continue;
+        }
+int box=(i/3)*3+(j/3);
+
+        if(rows[i].contains(c)||cols[j].contains(c)||boxs[box].contains(c)){return false;}
+        rows[i].add(c);
+        cols[j].add(c);
+        boxs[box].add(c);
+    }
+}
+        return true;
+    }
+}
